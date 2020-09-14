898c8558299220a30970e5fd7816d09c1d1dd2e3
yogita gupta <yggupta1112gmail.com>
2020-09-14 12:49:25 +0530

    complete frontend

---
+++ backend/data.js
@@ -0,0 +1,37 @@
+export default {
+    products :[
+        {
+            _id:'1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpeg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock :5
+        },
+        {
+            _id:'2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpeg',
+            price:80,
+            brand: 'Nike',
+            rating: 4.6,
+            numReviews: 10,
+            countInStock :4
+        },
+        {
+            _id:'3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d4.jpg',
+            price:70,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 10,
+            countInStock :6
+        },
+    ]
+}
